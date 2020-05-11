5e21dc2a35034adf1da7f2ca1e56a9edf67b1b15
Stephen Saciolo <stephensaciolo@Stephens-MBP.fios-router.home>
2020-05-11 15:33:25 -0400

    Created Repositories and Notes Tables

--- data/migrations/20200511132054_mission-control-tables.js.js
+++ data/migrations/20200511132054_mission-control-tables.js.js
@@ -51,7 +51,82 @@ exports.up = function (knex) {
         // Updated_At
         tbl.timestamp('updated_at').defaultTo(knex.fn.now())
       })
+
+      // Projects Table
+      .createTable('projects', (tbl) => {
+        // ID
+        tbl.increments()
+        // Name
+        tbl.string('name', 255).notNullable()
+        // Product Key (FK)
+        tbl
+          .integer('productKey')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('products')
+          .onDelete('RESTRICT')
+          .onUpdate('CASCADE')
+        // Active
+        tbl.boolean('active').notNullable()
+        // Created_At
+        tbl.timestamp('created_at').defaultTo(knex.fn.now())
+        // Updated_At
+        tbl.timestamp('updated_at').defaultTo(knex.fn.now())
+      })
+
+      // Repositories Table
+      .createTable('repositories', (tbl) => {
+        // ID
+        tbl.increments()
+        // URL
+        tbl.string('url', 255).notNullable()
+        // Product Key (FK)
+        tbl
+          .integer('productKey')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('products')
+          .onDelete('RESTRICT')
+          .onUpdate('CASCADE')
+      })
+
+      // Notes Table
+      .createTable('notes', (tbl) => {
+        // ID
+        tbl.increments()
+        // Project Key (FK)
+        tbl
+          .integer('projectKey')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('projects')
+          .onDelete('RESTRICT')
+          .onUpdate('CASCADE')
+        // Topic
+        tbl.string('topic', 255).notNullable()
+        // Content
+        tbl.string('content', 255).notNullable()
+        // Author
+        tbl
+          .integer('author')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('persons')
+          .onDelete('RESTRICT')
+          .onUpdate('CASCADE')
+      })
   )
 }
 
-exports.down = function (knex) {}
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('repositories')
+    .dropTableIfExists('projects')
+    .dropTableIfExists('products')
+    .dropTableIfExists('programs')
+    .dropTableIfExists('persons')
+}
