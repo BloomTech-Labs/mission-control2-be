d39acb5fc00679c52de89d562974dea6402cb4ee
Stephen Saciolo <stephensaciolo@Stephens-MBP.fios-router.home>
2020-05-13 13:47:37 -0400

    Created Programs-Router and Programs-Model

---
+++ api/programs/programs-router.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const Data = require('./programs-model')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  Data.find()
+    .then((programs) => {
+      res.status(200).json(programs)
+    })
+    .catch((error) => {
+      console.log(error)
+      res.status(500).json({ message: 'Error retrieving programs' })
+    })
+})
+
+router.get('/:id', (req, res) => {
+  Data.findById(req.params.id)
+    .then((program) => {
+      program
+        ? res.status(200).json(program)
+        : res.status(404).json({ message: 'Program not found' })
+    })
+    .catch((error) => {
+      console.log(error)
+      res
+        .status(500)
+        .json({ message: 'Error retrieving the specified program' })
+    })
+})
+
+router.post('/', (req, res) => {
+  Data.add(req.body)
+    .then((program) => {
+      res.status(201).json(program)
+    })
+    .catch((error) => {
+      console.log(error)
+      res.status(500).json({ message: 'Error adding the program' })
+    })
+})
+
+router.put('/:id', (req, res) => {
+  Data.update(req.params.id, req.body)
+    .then((program) => {
+      program
+        ? res.status(200).json(program)
+        : res.status(404).json({
+            message: 'The program with the specified id could not be found',
+          })
+    })
+    .catch((error) => {
+      console.log(error)
+      res.status(500).json({ message: 'Error updating program' })
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  Data.remove(req.params.id)
+    .then((count) => {
+      count > 0
+        ? res.status(200).json({ message: 'This program has been removed' })
+        : res.status(404).json({ message: 'This program could not be found' })
+    })
+    .catch((error) => {
+      // log error to server
+      console.log(error)
+      res.status(500).json({
+        message: 'Error removing the program',
+      })
+    })
+})
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const helmet = require('helmet')
 const morgan = require('morgan')
 
 const personsRouter = require('./persons/persons-router')
+const programsRouter = require('./programs/programs-router')
 
 const server = express()
 
@@ -13,6 +14,7 @@ server.use(morgan('dev'))
 server.use(express.json())
 
 server.use('/api/persons', personsRouter)
+server.use('/api/programs', programsRouter)
 
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'We Up' })
