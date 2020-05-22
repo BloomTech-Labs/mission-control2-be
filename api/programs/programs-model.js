135615fdef57a0b420bd83cbc522490c666b4024
Stephen Saciolo <stephensaciolo@Stephens-MBP.fios-router.home>
2020-05-22 14:31:06 -0400

    Updated programs model and router to query products by program id

--- api/programs/programs-model.js
+++ api/programs/programs-model.js
@@ -1,33 +1,41 @@
-const db = require("../../data/db-config.js");
+const db = require('../../data/db-config.js')
 
 module.exports = {
   find,
   findById,
+  findProducts,
   add,
   update,
   remove,
-};
+}
 
 function find() {
-  return db("programs");
+  return db('programs')
 }
 
 function findById(id) {
-  return db("programs").where({ id }).first();
+  return db('programs').where({ id }).first()
+}
+
+function findProducts(id) {
+  return db('programs')
+    .join('products', 'programs.id', 'products.programKey')
+    .select('products.name', 'products.programKey', 'products.active')
+    .where('programKey', id)
 }
 
 function add(program) {
-  return db("programs")
-    .insert(program, "id")
+  return db('programs')
+    .insert(program, 'id')
     .then(([id]) => {
-      return this.findById(id);
-    });
+      return this.findById(id)
+    })
 }
 
 function update(id, changes) {
-  return db("programs").where({ id }).update(changes, "*");
+  return db('programs').where({ id }).update(changes, '*')
 }
 
 function remove(id) {
-  return db("programs").where({ id }).del();
+  return db('programs').where({ id }).del()
 }
